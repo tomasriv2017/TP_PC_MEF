e8e9b009290913b8137a61a65d7ef9c2ed813f59
tomasRiv2017 <tomyriv@hotmail.com>
2021-07-19 22:17:17 -0300

    agregado Behind Code(cs) para EstacionRecarga

--- dev/IA_MEF/EstacionRecarga.xaml.cs
+++ dev/IA_MEF/EstacionRecarga.xaml.cs
@@ -18,9 +18,19 @@ namespace IA_MEF
     /// </summary>
     public partial class EstacionRecarga : UserControl
     {
-        public EstacionRecarga()
+        public int X { get; private set; }
+        public int Y { get; private set; }
+        public EstacionRecarga(int x, int y)
         {
             InitializeComponent();
+            X = x;
+            Y = y;
+            TranslateTransform translate = new TranslateTransform(x, y);
+            RenderTransform = translate;
+            Height = 20;
+            Width = 20;
         }
+
     }
+
 }
