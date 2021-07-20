315d62e74cf53620da011597ad77fde30000c1d8
tomasRiv2017 <tomyriv@hotmail.com>
2021-07-19 22:06:16 -0300

    agregado behind code(cs) de Basura

--- dev/IA_MEF/Basura.xaml.cs
+++ dev/IA_MEF/Basura.xaml.cs
@@ -18,9 +18,24 @@ namespace IA_MEF
     /// </summary>
     public partial class Basura : UserControl
     {
-        public Basura()
+        public int X { get; private set; }
+        public int Y { get; private set; }
+
+        public Basura(int x, int y)
         {
             InitializeComponent();
+            X = x;
+            Y = y;
+            TranslateTransform translate = new TranslateTransform(x, y);
+            RenderTransform = translate;
+            Height = 20;
+            Width = 20;
         }
+
+        public void Recolectado()
+        {
+            Visibility = Visibility.Hidden;
+        }
+
     }
 }
